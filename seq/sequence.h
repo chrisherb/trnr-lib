693d50cb2b9d4afc9ce8cdb2ac9d2d2591c28989
Chris <chrisherb.sites@outlook.com>
2023-08-12 08:09:10 +0200

    some sequence wip

--- seq/sequence.h
+++ seq/sequence.h
@@ -5,23 +5,52 @@ namespace trnr {
 template <typename t_type, size_t t_max_length>
 // step sequence with arbitrary length and data
 class sequence {
+
+  enum direction { forward = 0, backward, pendulum };
+
 public:
-    std::array<t_type, length> m_data;
+  void set_data_point(size_t _index, t_type _data) { m_data[_index] = _data; }
+  void set_direction(direction _direction) { m_direction = _direction; }
+  void set_clock_division(int _division) { m_clock_division = _division; }
+  void set_length(int length) { m_length = length; }
 
-    t_type process_beat() {
-        t_type value = m_data[m_position];
+  t_type process_beat() {
+    t_type value = m_data[m_position];
 
-        m_position++;
-
-        if (m_position >= m_length) {
-            m_position = 0;
-        }
-
-        return value;
+    // advance position
+    if (m_position_count == 0) {
+      m_position++;
     }
 
+    // reset position
+    switch (m_direction) {
+    case forward:
+
+      break;
+    case backward:
+      break;
+    case pendulum:
+      break;
+    }
+    if (m_position >= m_length) {
+      m_position = 0;
+    }
+
+    // calculate clock division
+    m_position_count++;
+    if (m_position_count >= m_position) {
+      m_position_count = 0;
+    }
+
+    return value;
+  }
+
 private:
-    int m_position = 0;
-    int m_length = t_max_length;
+  std::array<t_type, length> m_data;
+  direction m_direction;
+  int m_position = 0;
+  int m_position_count = 0;
+  int m_clock_division = 1;
+  int m_length = t_max_length;
 };
-}
+} // namespace trnr
--- seq/sh_sequence.h
+++ seq/sh_sequence.h
@@ -4,90 +4,80 @@
 namespace trnr {
 
 struct note {
-    note(bool _note_on = false, int _pitch = 30, int _velocity = 100)
-    {
-        note_on = _note_on;
-        pitch = _pitch;
-        velocity = _velocity;
-    }
+  note(bool _note_on = false, int _pitch = 30, int _velocity = 100) {
+    note_on = _note_on;
+    pitch = _pitch;
+    velocity = _velocity;
+  }
 
-    bool note_on;
-    int pitch;
-    int velocity;
+  bool note_on;
+  int pitch;
+  int velocity;
 };
 
 template <size_t t_length>
 // sequence with chance control per step that samples incoming note information
 class sh_sequence : public sequence<int, t_length> {
 public:
-    int m_root = 30;
-    bool m_use_pitch;
-    bool m_use_octave;
-    bool m_use_velocity;
+  int m_root = 30;
+  bool m_use_pitch;
+  bool m_use_octave;
+  bool m_use_velocity;
 
-    std::vector<note> process_beat(int _pitch, int _octave, int _velocity, int _duration)
-    {
-        std::vector<note> notes;
-        int chance = chance_sequence.process_beat();
+  std::vector<note> process_beat(int _pitch, int _octave, int _velocity, int _duration) {
+    std::vector<note> notes;
+    int chance = chance_sequence.process_beat();
 
-        if (duration_left > 0)
-        {
-            duration_left--;
-        }
-        if (duration_left == 0)
-        {
-            note note_off = note(false, m_channel, m_sampled_pitch);
-            notes.push_back(note_off);
-            m_duration_left = -1; // reset
-        }
-
-        // 1 - 100
-        int num = rand() % 100;
-
-        if (num <= chance)
-        {
-            if (duration_left > 0)
-            {
-                // cut current note
-                note note_off = note(false, m_channel, m_sampled_pitch);
-                notes.push_back(note_off);
-                m_duration_left = -1; // reset
-            }
-
-            duration_left = duration;
-            note note_on = make_note(true, _pitch, _octave, _velocity);
-            notes.push_back(note_on);
-        }
-
-        return notes;
+    if (duration_left > 0) {
+      duration_left--;
     }
+    if (duration_left == 0) {
+      note note_off = note(false, m_channel, m_sampled_pitch);
+      notes.push_back(note_off);
+      m_duration_left = -1; // reset
+    }
+
+    // 1 - 100
+    int num = rand() % 100;
+
+    if (num <= chance) {
+      if (duration_left > 0) {
+        // cut current note
+        note note_off = note(false, m_channel, m_sampled_pitch);
+        notes.push_back(note_off);
+        m_duration_left = -1; // reset
+      }
+
+      duration_left = duration;
+      note note_on = make_note(true, _pitch, _octave, _velocity);
+      notes.push_back(note_on);
+    }
+
+    return notes;
+  }
 
 private:
-    int m_duration_left = -1;
-    int m_sampled_pitch = 0;
-    sequence<int, t_length> chance_sequence;
+  int m_duration_left = -1;
+  int m_sampled_pitch = 0;
+  sequence<int, t_length> chance_sequence;
 
-    note make_note(bool note_on, int pitch, int octave, int velocity)
-    {
-        int calculated_pitch = m_root;
-        int calculated_velocity = 100;
+  note make_note(bool note_on, int pitch, int octave, int velocity) {
+    int calculated_pitch = m_root;
+    int calculated_velocity = 100;
 
-        if (m_use_octave)
-        {
-            calculated_pitch += octave * 12;
-        }
-        if (m_use_pitch)
-        {
-            calculated_pitch += pitch;
-        }
-        if (m_use_velocity)
-        {
-            calculated_velocity = velocity;
-        }
-
-        m_sampled_pitch = calculated_pitch;
-
-        return note(note_on, calculated_pitch, calculated_velocity);
+    if (m_use_octave) {
+      calculated_pitch += octave * 12;
     }
+    if (m_use_pitch) {
+      calculated_pitch += pitch;
+    }
+    if (m_use_velocity) {
+      calculated_velocity = velocity;
+    }
+
+    m_sampled_pitch = calculated_pitch;
+
+    return note(note_on, calculated_pitch, calculated_velocity);
+  }
 };
-}
+} // namespace trnr
