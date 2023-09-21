8ad336d49e16670398fa7414d3ad8ec937c19f4d
Chris <chrisherb.sites@outlook.com>
2023-09-21 15:12:50 +0200

    added function to advance position

--- seq/sequence.h
+++ seq/sequence.h
@@ -6,51 +6,60 @@ template <typename t_type, size_t t_max_length>
 // step sequence with arbitrary length and data
 class sequence {
 
-  enum direction { forward = 0, backward, pendulum };
+	enum direction {
+		forward = 0,
+		backward,
+		pendulum
+	};
 
 public:
-  void set_data_point(size_t _index, t_type _data) { m_data[_index] = _data; }
-  void set_direction(direction _direction) { m_direction = _direction; }
-  void set_clock_division(int _division) { m_clock_division = _division; }
-  void set_length(int length) { m_length = length; }
+	void set_data_point(size_t _index, t_type _data) { m_data[_index] = _data; }
 
-  t_type process_beat() {
-    t_type value = m_data[m_position];
+	void set_direction(direction _direction) { m_direction = _direction; }
 
-    // advance position
-    if (m_position_count == 0) {
-      m_position++;
-    }
+	void set_clock_division(int _division) { m_clock_division = _division; }
 
-    // reset position
-    switch (m_direction) {
-    case forward:
+	void set_length(int length) { m_length = length; }
 
-      break;
-    case backward:
-      break;
-    case pendulum:
-      break;
-    }
-    if (m_position >= m_length) {
-      m_position = 0;
-    }
+	t_type process_beat()
+	{
+		t_type value = m_data[m_position];
 
-    // calculate clock division
-    m_position_count++;
-    if (m_position_count >= m_position) {
-      m_position_count = 0;
-    }
+		switch (m_direction) {
+		case forward:
+			advance_position(true);
+			// reset position
+			if (m_position >= m_length) { m_position = 0; }
+			break;
+		case backward:
+			advance_position(false);
+			break;
+		case pendulum:
+			break;
+		}
 
-    return value;
-  }
+		// calculate clock division
+		m_position_count++;
+		if (m_position_count >= m_position) { m_position_count = 0; }
+
+		return value;
+	}
 
 private:
-  std::array<t_type, length> m_data;
-  direction m_direction;
-  int m_position = 0;
-  int m_position_count = 0;
-  int m_clock_division = 1;
-  int m_length = t_max_length;
+	std::array<t_type, t_max_length> m_data;
+	direction m_direction = forward;
+	int m_position = 0;
+	int m_position_count = 0;
+	int m_clock_division = 1;
+	int m_length = t_max_length;
+
+	void advance_position(bool _forward)
+	{
+		int increment = _forward ? 1 : -1;
+		if (m_position_count == 0) {
+			if (_forward) m_position++;
+			else m_position--;
+		}
+	}
 };
 } // namespace trnr
