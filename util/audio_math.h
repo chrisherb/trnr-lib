90ce27a3740727a41c39388356642951acf35606
Chris <chris@ternar.tech>
2025-07-31 16:26:25 +0200

    fixed log calculation

--- util/audio_math.h
+++ util/audio_math.h
@@ -3,15 +3,13 @@
 #include <math.h>
 
 namespace trnr {
-	static inline double lin_2_db(double lin) {
-		return 20 * log(lin);
-	}
-
-	static inline double db_2_lin(double db) {
-		return pow(10, db/20);
-	}
-
-	static inline float midi_to_frequency(float midi_note) {
-		return 440.0 * powf(2.0, ((float)midi_note - 69.0) / 12.0);
-	}
+inline double lin_2_db(double lin)
+{
+	if (lin <= 1e-20) lin = 1e-20; // avoid log(0)
+	return 20.0 * log10(lin);
 }
+
+inline double db_2_lin(double db) { return pow(10.0, db / 20.0); }
+
+inline float midi_to_frequency(float midi_note) { return 440.0 * powf(2.0, ((float)midi_note - 69.0) / 12.0); }
+} // namespace trnr
